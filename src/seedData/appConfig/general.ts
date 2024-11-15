67e6dafd16fa5164524602265adf46e68c56ff43
Prince <princekizhthara@gmail.com>
2024-11-15 15:03:18 +0530

    feat(15): change general seed data

--- src/seedData/appConfig/general.ts
+++ src/seedData/appConfig/general.ts
@@ -1,27 +1,24 @@
 export const general = {
   name: "general",
   data: {
-    name: "general",
-    data: {
-      title: "FlexiShop",
-      icon: "https://img.icons8.com/?size=100&id=dxoYK8bxqiJr&format=png&color=000000",
-      description:
-        "Discover a world of shopping with FlexiShop, your go-to e-commerce app for a wide range of products. From electronics to fashion, find everything you need in one place with easy navigation, secure checkout, and fast delivery. Start shopping with FlexiShop today!",
-      keywords:
-        "e-commerce, online shopping, buy products online, electronics, fashion, home goods, beauty products, FlexiShop, shop online, best deals, secure checkout, fast delivery, wide selection, affordable prices",
-      author: "FlexiShop Team",
-      "og:title": "FlexiShop",
-      "og:description":
-        "Discover a world of shopping with FlexiShop, your go-to e-commerce app for a wide range of products. From electronics to fashion, find everything you need in one place with easy navigation, secure checkout, and fast delivery. Start shopping with FlexiShop today!",
-      "og:image":
-        "https://imageio.forbes.com/specials-images/imageserve/638a98b6a088e5ce47202972/0x0.jpg?format=jpg&height=900&width=1600&fit=bounds",
-      "twitter:card":
-        "https://media.sproutsocial.com/uploads/2015/07/twitter-cards.svg",
-      "twitter:title": "FlexiShop",
-      "twitter:description":
-        "Discover a world of shopping with FlexiShop, your go-to e-commerce app for a wide range of products. From electronics to fashion, find everything you need in one place with easy navigation, secure checkout, and fast delivery. Start shopping with FlexiShop today!",
-      "twitter:image":
-        "https://imageio.forbes.com/specials-images/imageserve/638a98b6a088e5ce47202972/0x0.jpg?format=jpg&height=900&width=1600&fit=bounds",
-    },
+    title: "FlexiShop",
+    icon: "https://img.icons8.com/?size=100&id=dxoYK8bxqiJr&format=png&color=000000",
+    description:
+      "Discover a world of shopping with FlexiShop, your go-to e-commerce app for a wide range of products. From electronics to fashion, find everything you need in one place with easy navigation, secure checkout, and fast delivery. Start shopping with FlexiShop today!",
+    keywords:
+      "e-commerce, online shopping, buy products online, electronics, fashion, home goods, beauty products, FlexiShop, shop online, best deals, secure checkout, fast delivery, wide selection, affordable prices",
+    author: "FlexiShop Team",
+    "og:title": "FlexiShop",
+    "og:description":
+      "Discover a world of shopping with FlexiShop, your go-to e-commerce app for a wide range of products. From electronics to fashion, find everything you need in one place with easy navigation, secure checkout, and fast delivery. Start shopping with FlexiShop today!",
+    "og:image":
+      "https://imageio.forbes.com/specials-images/imageserve/638a98b6a088e5ce47202972/0x0.jpg?format=jpg&height=900&width=1600&fit=bounds",
+    "twitter:card":
+      "https://media.sproutsocial.com/uploads/2015/07/twitter-cards.svg",
+    "twitter:title": "FlexiShop",
+    "twitter:description":
+      "Discover a world of shopping with FlexiShop, your go-to e-commerce app for a wide range of products. From electronics to fashion, find everything you need in one place with easy navigation, secure checkout, and fast delivery. Start shopping with FlexiShop today!",
+    "twitter:image":
+      "https://imageio.forbes.com/specials-images/imageserve/638a98b6a088e5ce47202972/0x0.jpg?format=jpg&height=900&width=1600&fit=bounds",
   },
 };
