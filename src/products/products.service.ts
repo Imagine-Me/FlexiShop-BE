f47bd622851e07ee9fe08dab7db9976d3dc02761
Prince <princekizhthara@gmail.com>
2024-11-27 11:10:16 +0530

    fix(33): add product

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { CreateProductDto } from "./dto/create-product.dto";
-// import { UpdateProductDto } from "./dto/update-product.dto";
+import { UpdateProductDto } from "./dto/update-product.dto";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Product } from "./entities/product.entity";
 import { Repository } from "typeorm";
@@ -59,11 +59,8 @@ export class ProductsService {
     return `This action returns a #${id} product`;
   }
 
-  update(
-    id: number,
-    //  updateProductDto: UpdateProductDto
-  ) {
-    return `This action updates a #${id} product`;
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product ${updateProductDto}`;
   }
 
   remove(id: number) {
